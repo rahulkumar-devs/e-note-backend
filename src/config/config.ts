7359dc698f09e4630e68c769056bd675c258a45b
rahul kumar <rahulkumar9142684664@gmail.com>
2024-04-12 21:02:40 +0530

    add refresh&access token

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,22 +1,27 @@
-import "dotenv/config"
-
+import "dotenv/config";
 
 // _config make it private
-const _config ={
-    port: process.env.PORT,
-    mongoDB_uri : process.env.MONGODB_URI as string,
-    env:process.env.NODE_ENV ,
-    redis_uri : process.env.REDIS_URI,
-    cloud_name:process.env.CLOUD_NAME,
-    api_key:process.env.API_KEY,
-    api_secret_key:process.env.API_SECRET_KEY,
-    jwt_secret_key:process.env.JWT_SECRET_KEY as string,
+const _config = {
+   port: process.env.PORT,
+   mongoDB_uri: process.env.MONGODB_URI as string,
+   env: process.env.NODE_ENV,
+   redis_uri: process.env.REDIS_URI,
+   cloud_name: process.env.CLOUD_NAME,
+   api_key: process.env.API_KEY,
+   api_secret_key: process.env.API_SECRET_KEY,
+   jwt_secret_key: process.env.JWT_SECRET_KEY as string,
 
-    // google crediential
-    google_client_id:process.env.GOOGLE_CLIENT_ID as string,
-   google_client_secret :process.env.GOOGLE_CLIENT_SECRET as string,
-   express_session_secret_key:process.env.EXPRESS_SESSION_SECRET_KEY as string
-}
+   // google crediential
+   google_client_id: process.env.GOOGLE_CLIENT_ID as string,
+   google_client_secret: process.env.GOOGLE_CLIENT_SECRET as string,
+   express_session_secret_key: process.env.EXPRESS_SESSION_SECRET_KEY as string,
+
+   //    tokens details
+   access_token_key: process.env.REFRESH_TOKEN_KEY as string,
+   refresh_token_key: process.env.ACCESS_TOKEN_KEY as string,
+   access_token_expiry: process.env.REFRESH_TOKEN_EXPIRY as string,
+   refresh_token_expiry: process.env.ACCESS_TOKEN_EXPIRY as string,
+};
 
 // freez _config so that noBody can overWrite
 export const config = Object.freeze(_config);
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,6 +2,8 @@
 import mongoose, { Document, Schema, Model } from "mongoose";
 import bcrypt from "bcryptjs";
 import createHttpError from "http-errors";
+import jwt from "jsonwebtoken";
+import { config } from "../config/config";
 
 export interface IUser extends Document {
    name: string;
@@ -12,6 +14,7 @@ export interface IUser extends Document {
       url: string;
    };
    isComparePassword(password: string): boolean;
+   generateAccess(password: string): boolean;
 }
 
 const UserSchema = new mongoose.Schema(
@@ -32,23 +35,40 @@ const UserSchema = new mongoose.Schema(
 UserSchema.pre<IUser>("save", async function (next) {
    try {
       if (this.password && this.isModified("password")) {
-         this.password =  bcrypt.hashSync(
-            this.password,
-            bcrypt.genSaltSync(10)
-         );
+         this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(10));
       }
       next();
-   } catch (error:any) {
-      // Handle error properly, e.g., by logging it
+   } catch (error: any) {
       console.error("Error occurred during password hashing:", error);
-      // Pass the error to the callback function
+
       next(error);
    }
 });
 
-UserSchema.methods.isComparePassword = function (password: string):boolean {
+UserSchema.methods.isComparePassword = function (password: string): boolean {
    const isMatched = bcrypt.compareSync(password, this.password);
    return isMatched;
+};
+
+UserSchema.methods.generateAccessToken = async function () {
+   return jwt.sign(
+      {
+         _id: this._id,
+         email: this.email,
+         name: this.name,
+      },
+      config.access_token_key,
+      { expiresIn: config.access_token_expiry }
+   );
+};
+UserSchema.methods.generateRefreshToken = async function () {
+   return jwt.sign(
+      {
+         _id: this._id,
+      },
+      config.refresh_token_key,
+      { expiresIn: config.refresh_token_expiry }
+   );
 };
 
 // Make sure to use mongoose.model, not mongoose.Model
