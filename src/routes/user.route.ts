82370da540456d5b29d122bb8bf55cc3f41aaed9
rahul kumar <rahulkumar9142684664@gmail.com>
2024-04-12 14:41:12 +0530

    add Google authentication

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,11 @@
 
 import express from "express"
-import { createUser } from "../controllers/user.ctrl";
+import { createUser, loginUser } from "../controllers/user.ctrl";
 
 const userRouter = express.Router();
 
-userRouter.route("/register").post(createUser)
+userRouter.route("/register").post(createUser);
+userRouter.route("/login").post(loginUser);
 
 
 export default userRouter;
