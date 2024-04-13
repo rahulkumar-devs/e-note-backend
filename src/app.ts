73772d5878c1624ee14e64974b4363195fe68b09
rahul kumar <rahulkumar9142684664@gmail.com>
2024-04-13 09:01:15 +0530

    replace cookie-parser at the top

--- src/app.ts
+++ src/app.ts
@@ -6,18 +6,19 @@ import userRouter from "./routes/user.route";
 import compression from "compression";
 import shouldCompress from "./utils/shouldCompress";
 import morgan from "morgan";
-import cookieSession from "cookie-session";
+
 
 import passport from "passport";
 import cors from "cors";
 import path from "path";
 import expressSession from "express-session"
 import { config } from "./config/config";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-
+app.use(cookieParser())
 
 app.use(
    expressSession({
@@ -34,6 +35,7 @@ app.use(userRouter);
 
 app.use(cors());
 app.use(compression());
+
 
 // ejs template
 app.set("view engine", "ejs");
