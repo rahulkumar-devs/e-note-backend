dee62675e01620746eec3624d758672402d49cb0
rahul kumar <rahulkumar9142684664@gmail.com>
2024-04-21 10:14:31 +0530

    add book orutes

--- src/routes/books.route.ts
+++ src/routes/books.route.ts
@@ -1,6 +1,9 @@
 import express, { Router } from "express";
+import { isAuthenticated } from "../middlewares/authentication.middleware";
+import { restrict } from "../middlewares/rolePermission.middleware";
+import { createBook } from "../controllers/book.ctrl";
 
 const booksRoute:Router = express.Router();
 
-
+booksRoute.route('/create-book').post(isAuthenticated,restrict("admin,mentor"),createBook)
 export default booksRoute;
