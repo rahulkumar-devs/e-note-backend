90c297476b19f880cef2097b45fb341249dd53d1
rahul kumar <rahulkumar9142684664@gmail.com>
2024-04-21 10:14:04 +0530

    add book model

--- src/models/book.model.ts
+++ src/models/book.model.ts
@@ -1,41 +1,38 @@
 import expressAsyncHandler from "express-async-handler";
 import { IUser } from "./user.model";
-import mongoose, { Model,Types } from "mongoose";
+import mongoose, { Model, Types } from "mongoose";
 
-
-
-export interface IFile {
-
-}
+export interface IFile {}
 
 export interface IBook {
    _id?: string;
    title: string;
-   author: Types.ObjectId; 
+   author: Types.ObjectId;
    gener: string;
    coverImage: string;
    file: string;
-   thumbnail?:{};
-   like?:Array<string>;
-   dislike:Array<string>;
+   thumbnail?: {};
+   like?: Array<string>;
+   dislike: Array<string>;
    createdAt: Date;
    updatedAt: Date;
 }
 
-
-
-
 const bookSchema = new mongoose.Schema<IBook>(
    {
       title: { type: String },
-      author: { type: mongoose.Schema.Types.ObjectId, ref:"User",required: true },
+      author: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "User",
+         required: true,
+      },
       gener: { type: String },
       coverImage: { type: String },
       file: { type: String },
       createdAt: {
          type: Date,
       },
-      updatedAt: { type: Date, default: Date.now() },
+      updatedAt: { type: Date },
    },
    { timestamps: true }
 );
