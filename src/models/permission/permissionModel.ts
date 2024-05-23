e175794d7e7a39abeeaede1216c0d385cad249e3
rahul kumar <rahulkumar9142684664@gmail.com>
2024-05-23 14:28:21 +0530

    add permission

---
+++ src/models/permission/permissionModel.ts
@@ -0,0 +1,21 @@
+import mongoose, { Document, Schema, Types } from "mongoose";
+
+interface IPermission extends Document {
+   permission_name: string;
+   is_default: number;
+}
+
+const permissionSchema = new Schema<IPermission>({
+   permission_name: {
+      type: String,
+      required: true,
+   },
+   is_default: {
+      type: Number,
+      default:0,
+   },
+});
+
+const Permission = mongoose.model<IPermission>('Permission', permissionSchema);
+
+export default Permission;
